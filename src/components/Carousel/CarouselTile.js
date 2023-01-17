bfc89154f1e65a58cb7ffde2a4355c2aa4724cff
neilmbarry <neilmbarry@gmail.com>
2023-01-17 09:44:56 -0500

    finished host page

--- src/components/Carousel/CarouselTile.js
+++ src/components/Carousel/CarouselTile.js
@@ -1,7 +1,9 @@
 import React from "react";
 import classes from "./CarouselTile.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-const CarouselTile = ({ picture, title, fraction, taller, width }) => {
+const CarouselTile = ({ picture, title, fraction, taller, width, product }) => {
   const tileWidth = (width || 1000) / fraction - 12;
   const style = { width: `${tileWidth}px` };
   return (
@@ -9,7 +11,17 @@ const CarouselTile = ({ picture, title, fraction, taller, width }) => {
       <div className={taller ? classes.picTaller : classes.pic}>
         <img src={picture} alt="" />
       </div>
-      {title && <h4>{title}</h4>}
+      {title && <h4 className={classes.title}>{title}</h4>}
+      {product && (
+        <div className={classes.product}>
+          <h3 className={classes.prodTitle}>{product.title}</h3>
+          <div className={classes.ratingBox}>
+            <h4>{product.rating || "unrated"}</h4>
+            <FontAwesomeIcon icon={faStar} className={classes.star} />
+            <h4>({product.reviews || "No"} trips)</h4>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/host/HostPage.js
+++ src/pages/host/HostPage.js
@@ -6,11 +6,40 @@ import HostImage from "./HostImage/HostImage";
 import photo from "../../img-compressed/992-can-2x.jpg";
 import HostAdditional from "./HostAdditional/HostAdditional";
 import HostComponent from "./HostComponent/HostComponent";
+import CarouselTile from "../../components/Carousel/CarouselTile";
+import ProductReviews from "../product/ProductReviews/ProductReviews";
 
 const hostInfo = {
   image: photo,
   rating: 5,
 };
+
+const reviewsTemp = [
+  {
+    rating: 5,
+    reviewer: "Neil B.",
+    review:
+      "Amazing car with an amazing host no problems at all car was super clean fun and luxury at the same pace host was fantastic with communication will defiantly rent again. Thank you",
+    image: photo,
+    dateAdded: "June 20, 2022",
+  },
+  {
+    rating: 5,
+    reviewer: "Neil B.",
+    review:
+      "Amazing car with an amazing host no problems at all car was super clean fun and luxury at the same pace host was fantastic with communication will defiantly rent again. Thank you",
+    image: photo,
+    dateAdded: "June 20, 2022",
+  },
+  {
+    rating: 5,
+    reviewer: "Neil B.",
+    dateAdded: "June 20, 2022",
+    review:
+      "Amazing car with an amazing host no problems at all car was super clean fun and luxury at the same pace host was fantastic with communication will defiantly rent again. Thank you",
+    image: photo,
+  },
+];
 
 const HostPage = ({ className }) => {
   const classesList = `${classes.main} ${className}`;
@@ -21,40 +50,55 @@ const HostPage = ({ className }) => {
         <img src={banner} alt="" className={classes.bannerImg} />
       </div>
       <div className={classes.pageContent}>
-        <div className={classes.titleContent}>
-          <HostImage hostInfo={hostInfo} className={classes.hostImage} />
-          <div className={classes.titleBox}>
-            <h2>Neil B.</h2>
-            <HostComponent title="Toronto, CA">
-              <p>4819 trips - Joined Dec 2020</p>
-            </HostComponent>
+        <div className={classes.left}>
+          <div className={classes.titleContent}>
+            <HostImage hostInfo={hostInfo} className={classes.hostImage} />
+            <div className={classes.titleBox}>
+              <h2>Neil B.</h2>
+              <HostComponent title="Toronto, CA">
+                <p>4819 trips - Joined Dec 2020</p>
+              </HostComponent>
+            </div>
+            <HostAdditional />
           </div>
-          <HostAdditional />
+          <HostComponent title="Verified Info" className={classes.verified}>
+            <p>Email</p>
+          </HostComponent>
+          <HostComponent title="Languages" className={classes.lang}>
+            <p>English</p>
+          </HostComponent>
+          <HostComponent title="Works" className={classes.works}>
+            <p>Downtown Toronto</p>
+          </HostComponent>
+          <HostComponent title="School" className={classes.school}>
+            <p>University of Toronto</p>
+          </HostComponent>
+          {/* <HostComponent title="Host stats" className={classes.stats}>
+            <p>Placeholder</p>
+          </HostComponent> */}
         </div>
-        <HostComponent title="Verified Info" className={classes.verified}>
-          <p>Placeholder</p>
-        </HostComponent>
-        <HostComponent title="Languages" className={classes.lang}>
-          <p>Placeholder</p>
-        </HostComponent>
-        <HostComponent title="Works" className={classes.works}>
-          <p>Placeholder</p>
-        </HostComponent>
-        <HostComponent title="School" className={classes.school}>
-          <p>Placeholder</p>
-        </HostComponent>
-        <HostComponent title="Host stats" className={classes.stats}>
-          <p>Placeholder</p>
-        </HostComponent>
-        <HostComponent title="About Vincent" className={classes.about}>
-          <p>Placeholder</p>
-        </HostComponent>
-        <HostComponent title="Vincent's garments" className={classes.products}>
-          <p>Placeholder</p>
-        </HostComponent>
-        <HostComponent title="Reviews from guests" className={classes.reviews}>
-          <p>Placeholder</p>
-        </HostComponent>
+        <div className={classes.right}>
+          <HostComponent title="About Vincent" className={classes.about}>
+            <p>
+              Freelance Journalist, Screenwriter, Customer service agent,
+              Co-founder of the webseries Jada and Rémi, a mixed couple (An
+              African girl and a Quebecer).
+            </p>
+          </HostComponent>
+          <HostComponent
+            title="Vincent's garments"
+            className={classes.products}
+          >
+            <CarouselTile
+              picture={photo}
+              taller={true}
+              product={{ title: "My car", rating: 4.9, reviews: 235 }}
+            />
+          </HostComponent>
+          <HostComponent title="" className={classes.reviews}>
+            <ProductReviews reviews={reviewsTemp} rating={4.9} />
+          </HostComponent>
+        </div>
       </div>
     </div>
   );

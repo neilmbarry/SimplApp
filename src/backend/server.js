ac52f391b3926156ffca13f23c984bae0e79dfe1
neilmbarry <neilmbarry@gmail.com>
2023-01-24 11:59:41 -0500

    minor fix

--- src/backend/server.js
+++ src/backend/server.js
@@ -10,7 +10,7 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.set("strictQuery", true);
+mongoose.set("strictQuery", false);
 
 mongoose.connect(DB).then(() => {
   console.log("Connected to MongoDB");
